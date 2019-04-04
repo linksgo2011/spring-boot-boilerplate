1547f5658d33bc1c5a37b9207a76e39301f796dd
Lin Ning <nlin@thoughtworks.com>
2019-04-04 12:00:01 +0800

    First commit for the project

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # spring-boot-boilerplate
 Launch a spring boot based project quickly with basic features 
+
+## features
+
+- actuator
+- aop
+- cache
+- JPA
+- HATEOAS
+- mail
+- webflux
+- flyway
+- session
+- lombok
+- JMS
+
+
---
+++ src/main/java/cn/printf/springbootboilerplate/SpringBootBoilerplateApplication.java
@@ -0,0 +1,13 @@
+package cn.printf.springbootboilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBoilerplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBoilerplateApplication.class, args);
+	}
+
+}
